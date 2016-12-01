292033716b1b212b81fe8109ee6d586fce386796
yonglian qian <3321747055@qq.com>
2016-12-01 22:23:01 +0800

    First Github

---
+++ ConsoleApplication20161201/ConsoleApplication20161201/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication20161201
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 2 ; i < 100; i++)
+            {
+                int k = 0;
+                for (int j = 2; j <= (int)Math.Sqrt(i); j++)
+                {
+                    if (i%j==0)
+                    {
+                        k++;
+                    }
+                }
+                if (k>0)
+                {
+                    continue;
+                }
+                else
+                {
+                    sum += i;
+                }
+            }
+            Console.Write("总和{0}",sum);
+        }
+    }
+}
